bd8415739bd525a3ca306084962a426ffcac5d4a
Elena Shovtyuk <110084590+elenashovtyuk@users.noreply.github.com>
2022-11-29 14:40:27 +0400

    add permission_classes in CatViewSet

--- cats/views.py
+++ cats/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework import permissions
 
 from .models import Achievement, Cat, User
 
@@ -9,8 +10,15 @@ class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
 
+    # для того, чтобы настроить права доступа на уровне представлений,
+    # импортируем модуль permissions и
+    # добавляем к вьюсету новый аттрибут с кортежем пермишенов(разрешений)
+    # после этого получить список котиков может даже анонимнеый пользователь,
+    # но внести какие-либо изменения анонимный польз-ль не может
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
     def perform_create(self, serializer):
-        serializer.save(owner=self.request.user) 
+        serializer.save(owner=self.request.user)
 
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
